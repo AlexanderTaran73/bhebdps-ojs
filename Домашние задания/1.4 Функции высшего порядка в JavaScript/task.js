00099162246b88070fe4fa780c3b48c96fecb79f
Alexander Taran <ataran@ultimeta.ru>
2025-11-18 01:28:59 +0300

    DZ 1.4

--- Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
+++ Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
@@ -1,9 +1,54 @@
-//Задача № 1
+// Задача №1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  return function wrapper(...args) {
+    const hash = md5(args);
+    
+    const obj = cache.find(item => item.hash === hash);
+    if (obj) {
+      console.log("Из кеша: " + obj.value);
+      return "Из кеша: " + obj.value;
+    }
+
+    const result = func(...args);
+    cache.push({ hash: hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
-//Задача № 2
+
+// Задача №2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isFirstCall = true;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (isFirstCall) {
+      func(...args);
+      wrapper.count++;
+      isFirstCall = false;
+    }
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
+
